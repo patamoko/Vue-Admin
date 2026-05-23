db193873622743c89ac25f508d1060a4e3867a5a
patamoko <3284016211@qq.com>
2026-05-23 09:15:03 +0800

    自动同步：2026-05-23 09:15:03

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,3 +1,16 @@
+import { reactive} from "vue";
 
-export function usePagination( loadData:()=>Promise<any> ){//分页hooks
+export function usePagination( loadData:()=>Promise<any>,initalPageSize=10){
+    const pageInfo=reactive({
+        page:1,
+        pageSize:initalPageSize
+    })
+    const handleSizeChange=(size:number)=>{
+        pageInfo.pageSize=size;
+        loadData()
+    }
+    const handleCurrentChange=(page:number)=>{
+        pageInfo.page=page;
+        loadData()
+    }
 }
